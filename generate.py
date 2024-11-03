e107e2a72938a842062fc9d9f90b84c3de39193c
Marc777777777777 <m7rc.k7sp7r@gmail.com>
2024-11-03 18:28:52 +0100

    Add files via upload

--- generate.py
+++ generate.py
@@ -1,4 +1,4 @@
-import torch 
+import torch
 import torchvision
 import os
 import argparse
@@ -7,6 +7,11 @@ import argparse
 from model import Generator
 from utils import load_model
 
+def load_decoder(D, folder):
+    ckpt = torch.load(os.path.join(folder,'D.pth'))
+    D.load_state_dict({k.replace('module.', ''): v for k, v in ckpt.items()})
+    return D
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Generate Normalizing Flow.')
     parser.add_argument("--batch_size", type=int, default=2048,
@@ -14,6 +19,8 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
 
+    path = 'checkpoints'
+    epsilon = 0.01
 
 
     print('Model Loading...')
@@ -25,8 +32,20 @@ if __name__ == '__main__':
     model = torch.nn.DataParallel(model).cuda()
     model.eval()
 
+
+    decoder = Discriminator(mnist_dim).cuda()
+    decoder = load_decoder(decoder,path)
+    decoder = torch.nn.DataParallel(decoder).cuda()
+    decoder.eval()
+
     print('Model loaded.')
 
+    #Calculating M
+    z = torch.randn(10000, 100).cuda()
+    x = model(z)
+    output = decoder(x)
+    D_bar = output/(1-output)
+    M = torch.quantile(D_bar, 0.95)
 
 
     print('Start Generating')
@@ -37,11 +56,17 @@ if __name__ == '__main__':
         while n_samples<10000:
             z = torch.randn(args.batch_size, 100).cuda()
             x = model(z)
+            output = decoder(x)
+            D_bar = torch.log(output/(1-output))
+            p = torch.sigmoid(D_bar - torch.log(M) - torch.log(1-torch.exp(D_bar-torch.log(M)-epsilon)))
+            phi = torch.rand(args.batch_size)
             x = x.reshape(args.batch_size, 28, 28)
             for k in range(x.shape[0]):
                 if n_samples<10000:
-                    torchvision.utils.save_image(x[k:k+1], os.path.join('samples', f'{n_samples}.png'))         
-                    n_samples += 1
+                    if phi[k] <= p[k]:
+                        torchvision.utils.save_image(x[k:k+1], os.path.join('samples', f'{n_samples}.png'))
+                        n_samples += 1
 
 
-    
+
+
